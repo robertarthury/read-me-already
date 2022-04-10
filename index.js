c7c6e3bdc53acb348820fa40cc2e2b485745e3ab
robertarthury <robertarthury@gmail.com>
2022-04-10 19:20:06 -0400

    getting close

--- index.js
+++ index.js
@@ -1,112 +1,24 @@
-const inquirer = require('inquirer');
-const fs = require('fs')
-const util = require
-const generatePage = require('./generateMarkdown.js');
-const { writeFile, copyFile } = require('./README.md');
-const commandLineArgs = process.argv.slice(2, process.argv.length);
-    console.log(commandLineArgs);
+// Variables containing required packages
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+// Variables to connect modules to application
+const generateMarkdown = require("/Users/robertyoung/bootCamp/potential-enigma/Develop/utils/generateMarkdown.js");
+const licenseBadge = require("/Users/robertyoung/bootCamp/potential-enigma/Develop/utils/licenseBadge.js").licenseBadge;
+const questions = require("/Users/robertyoung/bootCamp/potential-enigma/Develop/utils/questions.js").questions;
+//Allows for use of async await
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create an array of questions for user input
-const promptUser = () => {
-    return inquirer.prompt[(
-        {
-            type: 'input',
-            name: 'projectName',
-            message: "What is your project's name? (Required)",
-            validate: projectNameInput => {
-                if (projectNameInput) {
-                    return true;
-                } else {
-                    console.log("Please enter a project name!");
-                    return false;
-                    }
-                }
-        },
-        {
-            tpye: 'input',
-            name: 'describtion',
-            message: 'Please provide a description of your new project. (Required)',
-            validate: describtionInput => {
-                if (describtionInput) {
-                  return true;
-                } else {
-                  console.log('Please enter a describtion of the project!');
-                  return false;
-                }
-             }
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username (Required)',
-            validate: githubInput => {
-              if (githubInput) {
-                return true;
-              } else {
-                console.log('Please enter your GitHub username!');
-                return false;
-              }
-            }
-        },
-        {
-            type: 'input',
-            name: 'usageInput',
-            message: 'Would you like to include information about the usage of the project?',
-            default: false
-        },
-        {
-            type: 'input',
-            name: 'testInput',
-            message: 'Which testing suites where used to create the project?',
-            default: false
-        },
-        {
-            type: 'input',
-            name: 'contributeInput',
-            message: 'Any other people contributing to this project?',
-            default: false
-        },
-        {
-            type: 'input',
-            name: 'emailInput',
-            message: 'What is your email address? (Required)',
-            validate: emailInput => {
-                if (emailInput) {
-                  return true;
-                } else {
-                  console.log('Please enter your email address!');
-                  return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'installationInput',
-            message: 'Are there any dependencies required for installation?',
-            default: false
-        },
-        {
-            type: 'list',
-            name: "licenseInput",
-            message: "Would you like to choose a lisence that the project is covered under?",
-            choices: [
-                'none',
-                'The MIT License', //= [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]
-                'GNU General Public License' //[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]
-            ]
-        }
-    )]
+// create a function to initialize program and create README file
+async function init() {
+  try {
+    const answers = await inquirer.prompt(questions);
+    answers.licenseBadge = licenseBadge(answers.license);
+    let readMeData = generateMarkdown(answers);
+    await writeFileAsync("created-README.md", readMeData);
+  } catch (err) {
+    throw err;
+  }
 }
-// TODO: Create a function to write README file
-
-
-
-// Function call to initialize app
-//init();
-
-promptUser()
-  .then((data) => writeFileAsync ('README.md',
-  generateMarkdown(data)))
-    .then(() => console.log('Success!')
-  .catch((err) => {
-    console.log(err)};
+//call the function
+init();
